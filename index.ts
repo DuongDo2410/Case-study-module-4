e41bd1c48231153a82b325ba050476f063b5e894
quyendch11 <quyendch11@gmail.com>
2022-08-18 08:11:37 +0700

    try catch file log err done

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import path from "path";
 import {router} from "./src/router/routes";
+import {errorHandler} from "./src/middleware/error";
 const app = express();
 //middle
 dotenv.config();
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.use(bodyParser.json());
 
 app.use("", router);
-
+app.use(errorHandler);
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log("sever is running port ", port);
